05b6b1839c513c9349d99545ca98514b45095e29
Agustín Stringa <stringaagustin1@gmail.com>
2023-02-18 14:44:20 -0300

    simulating v2 endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ const { categories_router } = require('./categories');
 const express = require('express');
 function routerApi(app) {
     const router = express.Router();
+    const routerv2 = express.Router();
+    routerv2.get('/', (req, res) => {
+        res.json({ message: "welcome to the v2" })
+    })
     app.use('/api/v1', router);
+    app.use('/api/v2', routerv2);
     app.get('/', function (req, res) {
         res.send('hello world')
     })
